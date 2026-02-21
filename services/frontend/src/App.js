0dafffff9a0ad451cb0aab9a63c0027801c68c87
Brenda Reine <rankoume.ir2026@esaip.org>
2026-02-21 22:09:08 +0100

    Modification du Frontend et de son manifest de déploiment pour corriger le bug Kubernete

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000";
+const API_BASE = process.env.REACT_APP_API_BASE || "";
+
 
 function App() {
   const [orders, setOrders] = useState([]);
